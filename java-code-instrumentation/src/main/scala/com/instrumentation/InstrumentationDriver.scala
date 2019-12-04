59706ebd839286cb70c3efa62aed1592e230de67
Abhijeet.Mohanty <mabhijeet1995@gmail.com>
2019-12-03 20:49:12 -0600

    Add statement ASTNode instead of method invocation ASTNode

--- java-code-instrumentation/src/main/scala/com/instrumentation/InstrumentationDriver.scala
+++ java-code-instrumentation/src/main/scala/com/instrumentation/InstrumentationDriver.scala
@@ -24,6 +24,8 @@ object InstrumentationDriver extends LazyLogging {
     val edits = customASTVisitor.getASTRewrite.rewriteAST(document, null)
     edits.apply(document)
 
+    println(document.get)
+
     val instrumentedCompilationUnit = parse(document.get.toCharArray)
   }
 
